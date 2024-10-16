ea306976398ca4de2fbb019e764e64b4b6a099ff
Danila Lepisa Mac <dadmoose@ya.ru>
2024-10-16 17:28:18 +0300

    Логика реализована согласно заданию

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function addCard(initialCards) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = initialCards.name;
+  cardElement.querySelector('.card__image').src = initialCards.link;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+
+  placesList.append(cardElement);
+}
+
+function removeCard(evt) {
+  evt.target.parentElement.remove();
+}
+
+initialCards.forEach(addCard);
+
