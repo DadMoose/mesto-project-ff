6ec3348071917dec79ec08cc6aa391f3e8ce295b
Danila Lepisa <DadMoose@ya.ru>
2024-10-16 22:44:23 +0300

    изменены названия констант, добавлен alt для картинок, разделены функции для создания и добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -9,22 +9,30 @@
 // @todo: Вывести карточки на страницу
 
 const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list');
+const cardsContainer = content.querySelector('.places__list');
+cardData = initialCards;
 
-function addCard(initialCards) {
+function createCard(cardData) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const buttonDelete = cardElement.querySelector('.card__delete-button');
 
-  cardElement.querySelector('.card__title').textContent = initialCards.name;
-  cardElement.querySelector('.card__image').src = initialCards.link;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.alt;
+  buttonDelete.addEventListener('click', removeCard);
 
-  placesList.append(cardElement);
+  return cardElement;
+}
+
+function addCard(cardData) {
+  const card = createCard(cardData);
+  cardsContainer.append(card);
 }
 
 function removeCard(evt) {
   evt.target.parentElement.remove();
 }
 
-initialCards.forEach(addCard);
+cardData.forEach(addCard);
 
