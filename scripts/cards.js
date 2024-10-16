8367355899d9165edd54c7680cbd04fbb53169b6
Danila Lepisa <DadMoose@ya.ru>
2024-10-16 22:39:25 +0300

    добавленÐÑ ключ-значение alt для картинок

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горные хребты Архыза"
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Ручей в зимнем лесу"
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Ряд панельных домов вечером, вид сверху"
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Камчатский пейзаж, земля поросшая мхом простирается до заснеженного вулкана"
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Одинокая железная дорога посреди леса вечером"
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Заснеженная скала острова Ольхон на Байкале"
     }
 ];
