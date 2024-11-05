9a95d51401941d9ce92cab786cc599d79a64848c
Данила Леписа <dadmoose@MacBook-Air-Danila.local>
2024-11-05 21:18:23 +0300

    реализовано редактирование имени и информации о себе

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,17 @@
 import {openModal, closeModal, handleCrossClick, handleEscKeyUp} from './components/modal'
 
 import '../pages/index.css';
-import { initialCards} from './cards';
+import {initialCards} from './cards';
 
 const content = document.querySelector('.content');
 const cardsContainer = content.querySelector('.places__list');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
-const popupTypeNewCard = document.querySelector('.popup_type_new-card')
-const popupTypeImage = document.querySelector('.popup_type_image')
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
 
 function createCard(cardData) {
   const cardTemplate = document.querySelector('#card-template').content;
@@ -37,6 +41,8 @@ initialCards.forEach(addCard);
 document.addEventListener('click', function (evt) {
   if (evt.target.classList.contains('profile__edit-button')) {
     openModal(popupTypeEdit);
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
   } else if (evt.target.classList.contains('profile__add-button')) {
     openModal(popupTypeNewCard);
   } else if (evt.target.classList.contains('card__image')) {
@@ -44,3 +50,11 @@ document.addEventListener('click', function (evt) {
   }
 });
 
+function handlePopupTypeEditSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(popupTypeEdit);
+}
+
+popupTypeEdit.addEventListener('submit', handlePopupTypeEditSubmit);
